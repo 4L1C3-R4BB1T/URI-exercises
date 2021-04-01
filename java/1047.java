b318b832df57791ad037b3de7967e3a2192ab3cb
Livia <68361379+4L1C3-R4BB1T@users.noreply.github.com>
2021-04-01 12:04:51 -0300

    fix

--- java/1047.java
+++ java/1047.java
@@ -5,27 +5,27 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
         
-        int startHour = sc.nextInt();
+		int startHour = sc.nextInt();
 		int startMinute = sc.nextInt();
-        
-        int finishHour = sc.nextInt();
-        int finishMinute = sc.nextInt();
-        
-        int difHours = finishHour - startHour;
-        int difMinutes = finishMinute - startMinute;
 
-        if (difMinutes < 0) { 
-            difHours--;
-            difMinutes += 60;
-        }
-        
-        if (difHours < 0) {
-            difHours += 24;
-        }
+		int finishHour = sc.nextInt();
+		int finishMinute = sc.nextInt();
 
-        if (difHours == 0 && difMinutes == 0) {
-            difHours = 24;
-        }
+		int difHours = finishHour - startHour;
+		int difMinutes = finishMinute - startMinute;
+
+		if (difMinutes < 0) { 
+		    difHours--;
+		    difMinutes += 60;
+		}
+
+		if (difHours < 0) {
+		    difHours += 24;
+		}
+
+		if (difHours == 0 && difMinutes == 0) {
+		    difHours = 24;
+		}
 
 		System.out.println("O JOGO DUROU "+ difHours + " HORA(S) E " + difMinutes + " MINUTO(S)");
 		
