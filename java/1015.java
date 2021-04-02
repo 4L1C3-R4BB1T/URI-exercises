e41ab353c413e7d6ee80691539ba26aba7355ea8
Livia <livinha.guimaraes.013@gmail.com>
2021-04-02 15:59:53 -0300

    Add solution to exercise 1015

---
+++ java/1015.java
@@ -0,0 +1,22 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+        
+        float x1 = sc.nextFloat();
+        float y1 = sc.nextFloat();
+        
+        float x2 = sc.nextFloat();
+        float y2 = sc.nextFloat();
+        
+        float distance = (float) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+ 
+        System.out.println(String.format("%.4f", distance));
+ 
+        sc.close();
+ 
+    }
+}
