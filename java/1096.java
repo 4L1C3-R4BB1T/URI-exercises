040d23af470fe4f60334f2b6e9c339333feef7df
Livia <livinha.guimaraes.013@gmail.com>
2021-04-17 16:28:09 -0300

    Add solution to exercise 1096

---
+++ java/1096.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+ 
+        for (int i = 1; i < 10; i +=2) {
+            for (int j = 7; j > 4; j--) {
+                System.out.println("I=" + i + " J=" + j);
+            }
+        }
+ 
+    }
+}
