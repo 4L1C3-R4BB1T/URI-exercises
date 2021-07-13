ad2cd81791c31729612a010b5a07e872eb6a2ded
Livia <livinha.guimaraes.013@gmail.com>
2021-07-13 15:53:15 -0300

    update

--- java/1183.java
+++ java/1183.java
@@ -7,26 +7,19 @@ public class Main {
         
         char op = sc.next().charAt(0);
         
-        float matriz[][] = new float[12][12];
-        
-        int flag = 1, cont = 0; 
+        double matriz[][] = new double[12][12];
         double sum = 0;
-        
-        if (op == 'S') cont = 1; 
-        
+       
         for (int i = 0; i < 12; i++) {
             for (int j = 0; j < 12; j++) {
-                matriz[i][j] = sc.nextFloat();
-                
-                if (j >= flag) {
-                    sum += matriz[i][j];
-                    if (op == 'M') cont++;
-                }
+                matriz[i][j] = sc.nextDouble();
+                if (j > i) sum += matriz[i][j];
             }
-            flag++;
         }
         
-        System.out.printf("%.1f\n", (sum/cont));
+        if (op == 'M') sum /= 66;
+        
+        System.out.printf("%.1f\n", sum);
         
         sc.close();
  
