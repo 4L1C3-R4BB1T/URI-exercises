a1ccc5b757ce4504b172b101b8ed053be769496b
Livia <livinha.guimaraes.013@gmail.com>
2021-07-31 13:52:56 -0300

    Add solution to exercise 2709

---
+++ java/2709.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+        
+        while (sc.hasNext()) {
+            int m = sc.nextInt();
+            
+            int coins[] = new int[m+1];
+            for (int i = 1; i <= m; i++) coins[i] = sc.nextInt();
+            
+            int n = sc.nextInt(), sum = 0;
+            
+            for (int j = 0; j*n < m; j++) sum += coins[m-j*n];
+            
+            boolean prime = true;
+            for (int k = 2; k < sum; k++) {
+                if (sum % k == 0) { prime = false; break; }
+            }
+            
+            System.out.println((prime && sum > 1) ? "You’re a coastal aircraft, Robbie, a large silver aircraft." : "Bad boy! I’ll hit you.");
+        }
+        
+        sc.close();
+ 
+    }
+}
