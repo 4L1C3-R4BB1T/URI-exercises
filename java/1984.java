dd6663a03b5f17e2ca6359d6b0d7b362ba5cc773
Livia <livinha.guimaraes.013@gmail.com>
2021-08-11 15:03:55 -0300

    Add solution to exercise 1984

---
+++ java/1984.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextLong();
+        String s = String.valueOf(n), inverted = "";
+		
+		for (int i = s.length()-1; i >= 0; i--) {
+			inverted += s.charAt(i);
+		}
+        
+        System.out.println(inverted);
+        
+        sc.close();
+ 
+    }
+}
