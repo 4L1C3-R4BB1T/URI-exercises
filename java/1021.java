11baf2a10d39bf65b153b4d4c40df5c9b2abc1b7
Livia <livinha.guimaraes.013@gmail.com>
2021-10-05 11:27:01 -0300

    Add solution to exercise 1021

---
+++ java/1021.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        
+        double valor = sc.nextDouble();
+        
+        int[] notas = {100, 50, 20, 10, 5, 2};
+        double[] moedas = {1, 0.50, 0.25, 0.10, 0.05, 0.01};
+        
+        System.out.println("NOTAS:");
+        for (int nota : notas) {
+        	int qtdNotas = (int) (valor/nota);
+        	valor %= nota;
+        	System.out.printf("%d nota(s) de R$ %.2f\n", qtdNotas, (double) nota);
+        }
+     
+        System.out.println("MOEDAS:");
+        for (double moeda : moedas) {
+            valor = Math.round(valor*100.0)/100.0;
+        	int qtdMoedas = (int) (valor/moeda);
+        	valor %= moeda;
+        	System.out.printf("%d moeda(s) de R$ %.2f\n", qtdMoedas, (double) moeda);
+        }
+        
+        sc.close();
+        
+    }
+}
