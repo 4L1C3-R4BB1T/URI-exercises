a9272e3ef7a9db244b6bcb9170efda45b3f291ea
Livia <livinha.guimaraes.013@gmail.com>
2021-10-10 15:30:22 -0300

    Add solution to exercise 2755

---
+++ java/2755.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+ 
+        System.out.printf("\"Ro'b'er\tto\\/\"\n" 
+            + "(._.) ( l: ) ( .-. ) ( :l ) (._.)\n"
+            + "(^_-) (-_-) (-_^)\n"
+            + "(\"_\") ('.')\n");
+
+    }
+}
