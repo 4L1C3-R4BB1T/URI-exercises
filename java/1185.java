f7081a077d21c86eddeb0d3e3c3bc5496a2a8920
Livia <livinha.guimaraes.013@gmail.com>
2021-12-06 19:02:54 -0300

    Add solution to exercise 1185

---
+++ java/1185.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+        
+        char o = sc.next().charAt(0);
+        
+        double m[][] = new double[12][12];
+        double sum = 0;
+        
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                m[i][j] = sc.nextDouble();
+                if (i + j < 11) sum += m[i][j];
+            }
+        }
+        
+        if (o == 'M') sum /= 66;
+        
+        System.out.printf("%.1f\n", sum);
+        
+        sc.close();
+        
+    }
+}
