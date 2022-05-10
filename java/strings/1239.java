1b0e69bfc8ad5e0bef3e269ee267617cf392dc04
Livia <livinha.guimaraes.013@gmail.com>
2022-05-09 21:29:22 -0300

    add solution to exercise 1239

---
+++ java/strings/1239.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            
+            // enquanto houverem _ ou *, eles serao substituidos
+            while (s.contains("_") || s.contains("*")) {
+				s = s.replaceFirst("_", "<i>")
+				    .replaceFirst("_", "</i>")
+				    .replaceFirst("\\*", "<b>")
+				    .replaceFirst("\\*", "</b>");
+			}
+            
+            System.out.println(s);
+        }
+        
+        sc.close();
+        
+    }
+}
